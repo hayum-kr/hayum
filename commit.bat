c10299d17c1f4c2f7f85e8a71876c295e416dbcc
대엽(덱스타) 이 <daeyub.lee@dexta.kr>
2026-06-24 10:58:35 +0900

    Add commit.bat

---
+++ commit.bat
@@ -0,0 +1,34 @@
+@echo off
+REM == Move to this batch file's folder (repo root) ==
+cd /d "%~dp0"
+
+REM ============ Git: pull -> commit -> push ============
+REM Commit message: from arg (commit.bat "msg"), else prompt, else date/time
+set "MSG=%~1"
+if "%MSG%"=="" set /p MSG=Commit message (Enter=auto):
+if "%MSG%"=="" set "MSG=update %DATE% %TIME%"
+
+echo [git] 1/4 pull (rebase, autostash) - get remote changes first
+git pull --rebase --autostash || goto :error
+
+echo [git] 2/4 add -A
+git add -A
+
+echo [git] 3/4 commit : %MSG%
+git commit -m "%MSG%"
+if errorlevel 1 echo [git] Nothing to commit, skipped
+
+echo [git] 4/4 push
+git push || goto :error
+
+echo.
+echo [git] Done
+pause
+goto :eof
+
+:error
+echo.
+echo [git] !!! Error - check messages above (conflict / auth / remote / network)
+echo   - On conflict: fix files, then  git rebase --continue   OR   git rebase --abort
+pause
+exit /b 1
